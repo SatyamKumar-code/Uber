7419b6e17816e4526e88194ec9a95cc930f6bc9a
SatyamKumar-code <satyamkumarsing59@gmail.com>
2026-04-20 22:51:50 +0530

    Refactor LiveTracking component to use Google Maps API instead of react-leaflet

--- frontend/src/components/LiveTracking.jsx
+++ frontend/src/components/LiveTracking.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
-// import { LoadScript, GoogleMap, Marker } from '@react-google-maps/api'
-import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
+import { LoadScript, GoogleMap, Marker } from '@react-google-maps/api'
+// import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
 
 
 const containerStyle = {
@@ -81,29 +81,29 @@ const LiveTracking = ({ className }) => {
     }, []);
 
     return (
-        // <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
-        //     <GoogleMap
-        //         mapContainerStyle={containerStyle}
-        //         center={currentPosition}
-        //         zoom={15}
-        //     >
-        //         <Marker position={currentPosition} />
-        //     </GoogleMap>
-        // </LoadScript>
+        <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={currentPosition}
+                zoom={15}
+            >
+                <Marker position={currentPosition} />
+            </GoogleMap>
+        </LoadScript>
 
-        <MapContainer
-            className={className || 'h-[65%]'}
-            center={currentPosition}
-            zoom={13}
-            scrollWheelZoom={true}
-        >
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <RecenterMap currentPosition={currentPosition} />
-            <Marker position={currentPosition} draggable eventHandlers={{ dragend: onDragEnd }} />
-        </MapContainer>
+        // <MapContainer
+        //     className={className || 'h-[65%]'}
+        //     center={currentPosition}
+        //     zoom={13}
+        //     scrollWheelZoom={true}
+        // >
+        //     <TileLayer
+        //         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        //         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        //     />
+        //     <RecenterMap currentPosition={currentPosition} />
+        //     <Marker position={currentPosition} draggable eventHandlers={{ dragend: onDragEnd }} />
+        // </MapContainer>
     )
 }
 
